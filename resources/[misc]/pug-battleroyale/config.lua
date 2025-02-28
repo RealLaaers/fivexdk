f800fce2d3389a9a17cb952bc5c7493248ed27db
Grisenxx <grisen328@gmail.com>
2025-02-28 01:05:35 +0100

    Update config.lua

--- resources/[misc]/pug-battleroyale/config.lua
+++ resources/[misc]/pug-battleroyale/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 Config.CoreName = 'es_extended' -- your core name
 ESX = exports["es_extended"]:getSharedObject()
-Config.Debug = true -- Debug true or false
+Config.Debug = false -- Debug true or false
 ----------
 ----------
 Config.JumpKey = 51 -- control id of the button to jump from the plane. Default: (E) [key codes can be found here: https://docs.fivem.net/docs/game-references/controls/]
@@ -170,33 +170,33 @@ Config.GulagMap = {
 Config.MapLocation = {
     ['Set_Dystopian_02'] = { 
         ['FlagSpawn'] = vector3(2793.11, -3796.54, 140.15),
-        [1] = vector3(2937.17, -3802.91, 144.25), -- Player 1 Spawn
-        [2] = vector3(2664.6, -3793.08, 143.71), -- Player 2 Spawn
+        [1] = vector3(2937.17, -3802.91, 244.25), -- Player 1 Spawn
+        [2] = vector3(2664.6, -3793.08, 243.71), -- Player 2 Spawn
     },
     ['Set_Dystopian_03'] = { 
         ['FlagSpawn'] = vector3(2800.2, -3796.79, 137.98),
-        [1] = vector3(2945.65, -3796.44, 143.26), -- Player 1 Spawn
-        [2] = vector3(2658.59, -3797.45, 143.58), -- Player 2 Spawn
+        [1] = vector3(2945.65, -3796.44, 243.26), -- Player 1 Spawn
+        [2] = vector3(2658.59, -3797.45, 243.58), -- Player 2 Spawn
     },
     ['Set_Dystopian_04'] = { 
         ['FlagSpawn'] = vector3(2780.02, -3788.88, 140.03),
-        [1] = vector3(2945.2, -3791.37, 140.03), -- Player 1 Spawn
-        [2] = vector3(2655.69, -3804.56, 140.03), -- Player 2 Spawn
+        [1] = vector3(2945.2, -3791.37, 240.03), -- Player 1 Spawn
+        [2] = vector3(2655.69, -3804.56, 240.03), -- Player 2 Spawn
     },
     ['Set_Dystopian_07'] = { 
         ['FlagSpawn'] = vector3(2802.17, -3806.3, 135.94),
-        [1] = vector3(2946.17, -3797.53, 140.1), -- Player 1 Spawn
-        [2] = vector3(2650.22, -3778.95, 140.14), -- Player 2 Spawn
+        [1] = vector3(2946.17, -3797.53, 240.1), -- Player 1 Spawn
+        [2] = vector3(2650.22, -3778.95, 240.14), -- Player 2 Spawn
     },
     ['Set_Dystopian_09'] = { 
         ['FlagSpawn'] = vector3(2828.07, -3799.42, 128.97),
-        [1] = vector3(2947.03, -3800.03, 139.63), -- Player 1 Spawn
-        [2] = vector3(2650.93, -3799.97, 139.68), -- Player 2 Spawn
+        [1] = vector3(2947.03, -3800.03, 239.63), -- Player 1 Spawn
+        [2] = vector3(2650.93, -3799.97, 239.68), -- Player 2 Spawn
     },
     ['Set_Dystopian_10'] = { 
         ['FlagSpawn'] = vector3(2812.87, -3791.98, 147.39),
-        [1] = vector3(2948.42, -3790.24, 139.96), -- Player 1 Spawn
-        [2] = vector3(2642.46, -3798.63, 139.97), -- Player 2 Spawn
+        [1] = vector3(2948.42, -3790.24, 239.96), -- Player 1 Spawn
+        [2] = vector3(2642.46, -3798.63, 239.97), -- Player 2 Spawn
     },
     -- ['Set_Wasteland_01'] = { 
     --     ['FlagSpawn'] = vector3(2946.13, -3795.33, 138.0),
