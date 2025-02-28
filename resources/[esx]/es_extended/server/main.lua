af42c95c350ab44f3a2fbbec6274f40486b2c3a8
Grisenxx <grisen328@gmail.com>
2025-02-28 01:41:32 +0100

    gjort så jeg kan double fivem

--- resources/[esx]/es_extended/server/main.lua
+++ resources/[esx]/es_extended/server/main.lua
@@ -96,13 +96,13 @@ if not Config.Multichar then
         local identifier = ESX.GetIdentifier(playerId)
 
         if identifier then
-            if ESX.GetPlayerFromIdentifier(identifier) then
-                deferrals.done(
-                    ('There was an error loading your character!\nError code: identifier-active\n\nThis error is caused by a player on this server who has the same identifier as you have. Make sure you are not playing on the same account.\n\nYour identifier: %s'):format(
-                        identifier))
-            else
+            -- if ESX.GetPlayerFromIdentifier(identifier) then
+            --     deferrals.done(
+            --         ('There was an error loading your character!\nError code: identifier-active\n\nThis error is caused by a player on this server who has the same identifier as you have. Make sure you are not playing on the same account.\n\nYour identifier: %s'):format(
+            --             identifier))
+            -- else
                 deferrals.done()
-            end
+            -- end
         else
             deferrals.done(
                 'There was an error loading your character!\nError code: identifier-missing\n\nThe cause of this error is not known, your identifier could not be found. Please come back later or report this problem to the server administration team.')
