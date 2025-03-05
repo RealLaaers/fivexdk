eff11c812df06669410b3bb0e05d389207bbc3d7
Grisenxx <grisen328@gmail.com>
2025-03-05 22:52:30 +0100

    koth tøj

---
+++ resources/[misc]/[eup]/fivexkoth/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with Durty Cloth Tool v3.1.0.3 (https://gta.clothing)
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_fivexkoth_shop.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_fivexkoth_shop.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
